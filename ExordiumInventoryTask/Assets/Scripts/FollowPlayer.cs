f74514f8aafc21538b16ed97a3de08570d06b405
Dominik Arih <da52617@fer.hr>
2022-08-09 19:57:03 +0200

    Add logic behind item pickup with raycasting
    Items are pick up-able with mouse click and/or trigger collision detection by pressing 'E'. Suitable message is displayed in logs.

---
+++ ExordiumInventoryTask/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform Player;
+    public Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (Player.position.x + Offset.x, Player.position.y + Offset.y, Player.position.z +Offset.z); 
+    }
+}
