64d7f7ed33a098532ac882bb60da120c07563628
Dominik Arih <da52617@fer.hr>
2022-08-10 13:32:42 +0200

    Create Inventory panel with logic behind slots and items
    Add scripts that define Item and Inventory as scriptable objects (ItemSO, Item, InventoryItem/InventorySO, Inventory Slots). Make Inventroy into prefab.

---
+++ ExordiumInventoryTask/Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Inventory.Model;
+
+public class Item : MonoBehaviour
+{
+    [field: SerializeField]
+    public ItemSO SingleItem {get; private set;}
+
+    [field: SerializeField]
+    public int Quantity {get;set;} = 1;
+
+    private void Start(){
+        GetComponent<SpriteRenderer>().sprite = SingleItem.ItemImage;
+    }
+
+    public void DestroyItem(){
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(gameObject);
+    }
+}
